3319e7e79ba317facfeed591610d6c4191271a86
alvarosena <alvarosenacs.c@gmail.com>
2023-02-06 15:43:26 -0300

    Save, list and get users in DB

--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
-
-users = []
 
 @api_view(['GET'])
 def endpoints(request):
@@ -16,23 +18,38 @@ def endpoints(request):
 def user_create(request):
     data = request.data
 
-    user = {
-        'username': data['username']
-    }
+    password_hashed = make_password(data['password'])
 
-    users.append(user)
+    user_exists = User.objects.filter(email=data['email'])
 
-    return Response(users)
+    if user_exists:
+        return Response({'Error': 'Email already taken'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        user = User(
+            username=data['username'], 
+            email=data['email'], 
+            password=password_hashed
+        )
 
+        user.save()  
+
+        serializer = UserSerializer(user)  
+
+        return Response(serializer.data)
 @api_view(['GET'])
 def user_list(request):
-    return Response(users)
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def user_get(request, username):
     
-    for user in users:
-        if user['username'] == username:
-            return Response(user)
+    user = User.objects.get(username=username)
+    if user:
+        serializer = UserSerializer(user)
 
-    return Response('User not found.')
+        return Response(serializer.data)
+    else:
+        return Response({'Error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
